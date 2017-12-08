2944cd44b4175c8c0874ff47e53a4c3dc0b7d05e
KANTAPONG SONG-NGAM <kantapong.so.59@ubu.ac.th>
2017-12-08 17:00:23 +0700

    Project Linux open person.mancity

--- ko/ko/cli.py
+++ ko/ko/cli.py
@@ -1,10 +1,23 @@
 import click
-
+import requests
+from PIL import Image
+from StringIO import StringIO
+from bs4 import BeautifulSoup
 
 @click.command()
-@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
-    """kok"""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+@click.argument('name', required=False)
+def main(name):
+    	URL='https://www.mancity.com/teams/first-team'
+	html=requests.get(URL)
+	b=BeautifulSoup(html.content,'html.parser')
+	search=b.find_all('div',{'class','squad-listing--person--image-inner feed-section--grey'})
+	for x in range(len(search)):
+		
+		S=str(search[x].picture.img['alt']).lower()
+		#print(S)
+		if name.lower()==S:
+			url=search[x].picture.img['data-src']
+			break
+	req=requests.get(url)
+    	img = Image.open(StringIO(req.content))
+    	img.show()
